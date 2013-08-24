f29bec18286c79cff6efc924c8b5fb12f0844dcf
Tomasz Ducin <tomasz.ducin@gmail.com>
2013-08-24 12:18:44 +0200

    translated MySQL comments to English

--- sql/model.sql
+++ sql/model.sql
@@ -1,27 +1,27 @@
 -- model script holds relational database tables definitions
 
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'klucz obcy',
-  `username` varchar(32) NOT NULL COMMENT 'login',
-  `password` varchar(255) NOT NULL COMMENT 'hasło',
-  `first_name` varchar(32) NOT NULL COMMENT 'imię',
-  `last_name` varchar(32) NOT NULL COMMENT 'nazwisko',
-  `email` varchar(127) DEFAULT NULL COMMENT 'adres e-mail',
-  `last_login` datetime DEFAULT NULL COMMENT 'ostatnio zalogowany',
-  `created_at` datetime DEFAULT NULL COMMENT 'data utworzenia',
-  `updated_at` datetime DEFAULT NULL COMMENT 'data ostatniej aktualizacji',
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'primary key',
+  `username` varchar(32) NOT NULL COMMENT 'user login',
+  `password` varchar(255) NOT NULL COMMENT 'user password',
+  `first_name` varchar(32) NOT NULL COMMENT 'user first name',
+  `last_name` varchar(32) NOT NULL COMMENT 'user last name',
+  `email` varchar(127) DEFAULT NULL COMMENT 'user email',
+  `last_login` datetime DEFAULT NULL COMMENT 'user last login time',
+  `created_at` datetime DEFAULT NULL COMMENT 'user account creation time',
+  `updated_at` datetime DEFAULT NULL COMMENT 'user account last update time',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='użytkownicy' AUTO_INCREMENT=1;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='application users' AUTO_INCREMENT=1;
 
 CREATE TABLE IF NOT EXISTS `notes` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'klucz główny',
-  `user_id` int(10) unsigned NOT NULL COMMENT 'klucz obcy: użytkownik, autor notatki',
-  `note_date` date NOT NULL COMMENT 'data',
-  `description` varchar(255) NOT NULL COMMENT 'opis',
-  `created_at` datetime DEFAULT NULL COMMENT 'data utworzenia',
-  `updated_at` datetime DEFAULT NULL COMMENT 'data ostatniej aktualizacji',
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'primary key',
+  `user_id` int(10) unsigned NOT NULL COMMENT 'foreign key: the user, note author',
+  `note_date` date NOT NULL COMMENT 'note date (it will be visible then)',
+  `description` varchar(255) NOT NULL COMMENT 'user note description',
+  `created_at` datetime DEFAULT NULL COMMENT 'user note creation time',
+  `updated_at` datetime DEFAULT NULL COMMENT 'user note last update time',
   INDEX user_id_note_date_idx (user_id, note_date),
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='notatki użytkowników' AUTO_INCREMENT=1;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='user notes' AUTO_INCREMENT=1;
 
 ALTER TABLE notes ADD CONSTRAINT note_user_id_users_id FOREIGN KEY (user_id) REFERENCES users(id);
