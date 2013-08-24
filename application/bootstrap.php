936a8e1a9e173e23baf5696222a3720f46e26acc
Tomasz Ducin <tomasz.ducin@gmail.com>
2013-08-24 13:11:30 +0200

    removed old kohana core directory

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -107,6 +107,8 @@ Kohana::modules(array(
 	// 'userguide'  => MODPATH.'userguide',  // User guide and API documentation
 	));
 
+Cookie::$salt = 'nqKiU782O2Djue555etmb1qIj5fjCbiD';
+
 /**
  * Set the routes. Each route must have a minimum of a name, a URI and a set of
  * defaults for the URI.
