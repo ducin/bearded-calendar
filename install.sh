f99a0fc0e253b6e3e6a4ea7e9d6eef8d9ff7b525
Tomasz Ducin <tomasz.ducin@gmail.com>
2013-08-24 13:37:24 +0200

    all kohana modules sources loaded as git submodules

--- install.sh
+++ install.sh
@@ -7,5 +7,11 @@ git submodule update
 cd modules/auth
 git checkout 3.2/develop
 
+cd ../modules/database
+git checkout 3.2/develop
+
+cd ../modules/orm
+git checkout 3.2/develop
+
 cd ../../system
 git checkout 3.2/develop
